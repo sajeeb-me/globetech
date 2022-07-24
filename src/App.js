abd47d4eaa7b9e53e521eecbf178eaac0226cabe
sdshuvo98 <hellosdshuvo@gmail.com>
2022-07-24 16:00:17 +0600

    seted up routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Services from './pages/Services/Services';
+import Contact from './pages/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <section className="App">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </section>
   );
 }
 
