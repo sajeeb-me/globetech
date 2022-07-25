0d8e8b4285fa170ea2d0f8c1dd22b5f34396ec5b
sdshuvo98 <hellosdshuvo@gmail.com>
2022-07-25 12:37:20 +0600

    login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import LoginForm from './LoginForm'
+import Back from '../../assets/icons/back.png'
 
 function Login() {
     return (
-        <div>Login</div>
+        <section className='grid grid-cols-4 min-h-screen'>
+            <article
+                style={{
+                    fontFamily: 'Montserrat'
+                }}
+                className='col-span-1 px-2 flex flex-col justify-between py-16'
+            >
+                <div>
+                    <h3 style={{ color: '#28ACE2' }} className='text-3xl uppercase font-bold'>Globetech Company Limited</h3>
+                    <p className='text-xs tracking-wider mt-5'>Make yourself digitalized &amp; more effecient</p>
+                </div>
+                <div className='flex justify-center'>
+                    <img className='h-6' src={Back} alt="" />
+                </div>
+            </article>
+            <article
+                style={{ background: '#C0CDD3' }}
+                className='col-span-3 flex justify-center items-center'
+            >
+                <div className='bg-white text-black rounded-xl p-10 w-1/2'>
+                    <LoginForm />
+                </div>
+            </article>
+        </section >
     )
 }
 
---
+++ src/pages/Login/LoginForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { EyeIcon, EyeOffIcon } from '@heroicons/react/solid'
+
+const LoginForm = () => {
+    const [showPass, setShowPass] = useState(false)
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+
+    return (
+        <section>
+            <article>
+                <h3 className="text-4xl mb-5">Welcome</h3>
+                <p className="text-sm my-5 opacity-50">Sign in to your account</p>
+            </article>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-3">
+                    <input
+                        className="border block p-3 rounded-lg w-full"
+                        placeholder="Email"
+                        type='email'
+                        {...register("email", { required: true })}
+                    />
+                    {errors.email?.type === 'required' && <p className="text-sm pl-2 pt-1 text-red-500 text-left">Email is required</p>}
+                </div>
+                <div className="mb-3 relative">
+                    <input
+                        className="border block p-3 rounded-lg w-full"
+                        placeholder="Password"
+                        type={showPass ? 'text' : 'password'}
+                        {...register("password", { required: true })}
+                    />
+                    <span
+                        className="absolute top-4 right-4 cursor-pointer"
+                        onClick={() => setShowPass(!showPass)}
+                    >
+                        {
+                            showPass ?
+                                <EyeOffIcon className="h-5 w-5 text-gray-500" />
+                                :
+                                <EyeIcon className="h-5 w-5 text-gray-500" />
+                        }
+                    </span>
+                    {errors.password?.type === 'required' && <p className="text-sm pl-2 pt-1 text-red-500 text-left">Password is required</p>}
+                </div>
+
+                <input
+                    type="submit"
+                    value='Login'
+                    style={{ background: '#28ACE2' }}
+                    className="p-3 rounded-lg w-full mt-3 text-white cursor-pointer"
+                />
+            </form>
+        </section>
+    );
+};
+
+export default LoginForm;
